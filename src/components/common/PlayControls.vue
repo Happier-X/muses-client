e82c7413a28c054d520f0c1fb927c0d9385bef44
赵鸿飞 <zhf5256@qq.com>
2025-08-14 13:21:09 +0800

    fix: 调整按钮大小

--- src/components/common/PlayControls.vue
+++ src/components/common/PlayControls.vue
@@ -1,20 +1,20 @@
 <template>
-  <n-flex align="center" justify="center" class="size-full">
-    <n-button quaternary circle :focusable="false">
-      <template #icon>
-        <n-icon class="i-mingcute-skip-previous-line"> </n-icon>
-      </template>
-    </n-button>
-    <n-button quaternary circle :focusable="false">
-      <template #icon>
-        <n-icon class="i-mingcute-play-line"> </n-icon>
-      </template>
-    </n-button>
-    <n-button quaternary circle :focusable="false">
-      <template #icon>
-        <n-icon class="i-mingcute-skip-forward-line"> </n-icon>
-      </template>
-    </n-button>
-  </n-flex>
+    <n-flex align="center" justify="center" class="size-full">
+        <n-button quaternary circle :focusable="false">
+            <template #icon>
+                <n-icon class="i-mingcute-skip-previous-line"></n-icon>
+            </template>
+        </n-button>
+        <n-button quaternary circle :focusable="false" size="large">
+            <template #icon>
+                <n-icon class="i-mingcute-play-line"></n-icon>
+            </template>
+        </n-button>
+        <n-button quaternary circle :focusable="false">
+            <template #icon>
+                <n-icon class="i-mingcute-skip-forward-line"></n-icon>
+            </template>
+        </n-button>
+    </n-flex>
 </template>
 <script setup lang="ts"></script>
