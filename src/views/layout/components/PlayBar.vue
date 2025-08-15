08975bb3aba3ac778fccd4692aa5cde0fcdd9c6e
赵鸿飞 <zhf5256@qq.com>
2025-08-15 15:40:03 +0800

    feat: 增加歌曲信息展示

--- src/views/layout/components/PlayBar.vue
+++ src/views/layout/components/PlayBar.vue
@@ -3,13 +3,24 @@
         <div class="absolute w-full left-0 top-[-0.5rem] z-1">
             <ProgressBar />
         </div>
-        <div class="size-full">
+        <n-flex align="center" class="size-full">
             <n-image
                 class="h-full aspect-square rounded-lg"
                 preview-disabled
                 object-fit="cover"
-                src="https://app.tngeek.com/api_sso/open/file/b4c64ae_cat-8321993_1280.webp" />
-        </div>
+                :src="
+                    playerStore.currentSongInfo?.cover ??
+                    'https://app.tngeek.com/api_sso/open/file/b4c64ae_cat-8321993_1280.webp'
+                " />
+            <n-flex vertical align="flex-start" size="small">
+                <n-text strong>
+                    {{ playerStore.currentSongInfo?.title ?? '未知标题' }}
+                </n-text>
+                <n-text :depth="3">
+                    {{ playerStore.currentSongInfo?.artist ?? '未知艺术家' }}
+                </n-text>
+            </n-flex>
+        </n-flex>
         <div class="size-full">
             <PlayControls />
         </div>
@@ -22,4 +33,6 @@
 import ProgressBar from '@/components/common/ProgressBar.vue'
 import PlayControls from '@/components/common/PlayControls.vue'
 import QueueControls from '@/components/common/QueueControls.vue'
+import { usePlayerStore } from '@/stores/player'
+const playerStore = usePlayerStore()
 </script>
