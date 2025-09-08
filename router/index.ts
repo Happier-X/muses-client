af6beff11b295efb79e3616f184779b08892db58
赵鸿飞 <zhf5256@qq.com>
2025-09-08 13:10:38 +0800

    fix: 调整图标

--- router/index.ts
+++ router/index.ts
@@ -5,7 +5,12 @@ const ignoreToken = [
 	"/pages/home/index",
 	"/pages/my/index",
 	"/pages/music/index",
-	"/pages/template/shop/address"
+	"/pages/music/songs/index",
+	"/pages/music/albums/index",
+	"/pages/music/artists/index",
+	"/pages/music/playlists/index",
+	"/pages/music/likes/index",
+	"/pages/music/recentlyPlayed/index"
 ];
 
 router.beforeEach((to, next) => {
