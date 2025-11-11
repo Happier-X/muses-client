9a5beb094365f62c857d1ce3dab418c3b365425f
赵鸿飞 <zhf5256@qq.com>
2025-11-11 09:39:29 +0800

    feat: 增加物理返回关闭弹窗功能

--- src/components/feature/Immersion.tsx
+++ src/components/feature/Immersion.tsx
@@ -1,3 +1,4 @@
+import { useBottomSheetBackHandler } from '@/hooks/useBottomSheetBackHandler'
 import { BottomSheetModal, BottomSheetView } from '@gorhom/bottom-sheet'
 import React, { forwardRef, useImperativeHandle, useRef } from 'react'
 import { Text } from 'react-native'
@@ -13,7 +14,7 @@ const Immersion = forwardRef<ImmersionType>((props, ref) => {
   useImperativeHandle(ref, () => ({
     open: () => bottomSheetModalRef.current?.present(),
   }))
-
+  const { handleSheetPositionChange } = useBottomSheetBackHandler(bottomSheetModalRef)
   return (
     <BottomSheetModal
       ref={bottomSheetModalRef}
@@ -23,6 +24,7 @@ const Immersion = forwardRef<ImmersionType>((props, ref) => {
       enableHandlePanningGesture={false}
       handleComponent={null}
       topInset={top}
+      onChange={handleSheetPositionChange}
     >
       <BottomSheetView style={{ height: '100%' }}>
         <Text>测试</Text>
